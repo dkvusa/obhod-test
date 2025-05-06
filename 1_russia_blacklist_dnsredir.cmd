9a5efb98db9be2d30f3d0465328e89b95d459d75
dkvusa <dangervulkan@gmail.com>
2025-05-06 08:23:55 +0300

    first appdata

---
+++ 1_russia_blacklist_dnsredir.cmd
@@ -0,0 +1,11 @@
+@ECHO OFF
+PUSHD "%~dp0"
+set _arch=x86
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set _arch=x86_64)
+IF DEFINED PROCESSOR_ARCHITEW6432 (set _arch=x86_64)
+PUSHD "%_arch%"
+
+start "" goodbyedpi.exe -e 2 -f 1 --reverse-frag --blacklist --dns-port 1253 --dnsv6-addr 2a02:6b8::feed:0ff --dnsv6-port 1253 --blacklist ..\russia-blacklist.txt --blacklist ..\russia-youtube.txt
+
+POPD
+POPD
